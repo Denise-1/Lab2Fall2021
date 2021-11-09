1db1436fc30e8b585508ff40919344bea0e70160
dcwhi <davidw5@umbc.edu>
2021-11-08 21:24:19 -0500

    changes1

--- src/Lab2/Print/Main.java
+++ src/Lab2/Print/Main.java
@@ -4,7 +4,7 @@ package Lab2.Print;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Johan");
+        System.out.println("Dave");
         int monthNumber;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number between 1 to 12.");
